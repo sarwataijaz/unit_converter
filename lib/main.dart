de95af14b3186fd9cda9afe784647ce197f3ec50
sarwataijaz <sarwataijaaz@gmail.com>
2024-07-15 20:51:48 +0500

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _selectedValue = 'Length';
+  String _selectedUnitValue = 'Length';
+  String _selectedLengthValue = 'Meter';
 
   List<String> _allUnits = ['Length', 'Area', 'Mass', 'Pressure'];
+  List<String> _lengthUnits = [
+    'Meter',
+    'Centimeter',
+    'Millimeter',
+    'Kilometer'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -45,124 +52,293 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       backgroundColor: Color(0xFFFEDD9E),
-      body: Container(
-        width: screenWidth,
-        height: screenHeight,
-        child: Padding(
-          padding: EdgeInsets.only(
-              top: 30.sp, bottom: 30.sp, left: 15.sp, right: 15.sp),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                    text: TextSpan(children: [
-                  TextSpan(
-                    text: "U",
-                    style: GoogleFonts.abyssinicaSil(
-                        textStyle: TextStyle(
-                            color: Color(0xFFCC800E),
-                            fontSize: 28.sp,
-                            decoration: TextDecoration.underline),
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: "nit ",
-                    style: GoogleFonts.abyssinicaSil(
-                        textStyle: TextStyle(
-                          color: Color(0xFFCC800E),
-                          fontSize: 26.sp,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          child: Padding(
+            padding: EdgeInsets.only(top: 25.sp, left: 15.sp, right: 15.sp),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                          text: "U",
+                          style: GoogleFonts.actor(
+                              textStyle: TextStyle(
+                                  color: Color(0xFFCC800E),
+                                  fontSize: 28.sp,
+                                  decoration: TextDecoration.underline),
+                              fontWeight: FontWeight.bold),
                         ),
-                        fontStyle: FontStyle.italic),
-                  ),
-                  TextSpan(
-                    text: "C",
-                    style: GoogleFonts.abyssinicaSil(
-                        textStyle: TextStyle(
-                            color: Color(0xFFCC800E),
-                            fontSize: 28.sp,
-                            decoration: TextDecoration.underline),
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: "onverter",
-                    style: GoogleFonts.abyssinicaSil(
-                        textStyle: TextStyle(
-                          color: Color(0xFFCC800E),
-                          fontSize: 26.sp,
+                        TextSpan(
+                          text: "nit  ",
+                          style: GoogleFonts.actor(
+                              textStyle: TextStyle(
+                                color: Color(0xFFCC800E),
+                                fontSize: 26.sp,
+                              ),
+                              fontWeight: FontWeight.w700),
                         ),
-                        fontStyle: FontStyle.italic),
+                        TextSpan(
+                          text: "C",
+                          style: GoogleFonts.actor(
+                              textStyle: TextStyle(
+                                  color: Color(0xFFCC800E),
+                                  fontSize: 28.sp,
+                                  decoration: TextDecoration.underline),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: "onverter",
+                          style: GoogleFonts.actor(
+                              textStyle: TextStyle(
+                                color: Color(0xFFCC800E),
+                                fontSize: 26.sp,
+                              ),
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ])),
+                      Padding(
+                        padding: EdgeInsets.only(left: 12.0.sp),
+                        child: Image.asset(
+                          'assets/iconsssss.png',
+                          height: 10.h,
+                          width: 10.w,
+                        ),
+                      ),
+                    ],
                   ),
-                ])),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0.sp),
-                  child: Image.asset(
-                    'assets/iconsssss.png',
-                    height: 12.h,
-                    width: 12.w,
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: 30.sp, bottom: 30.sp, left: 15.sp, right: 15.sp),
-              child: Container(
-                width: screenWidth,
-                height: 13.h,
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Color(0xFFFFF1D4)),
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: 15.sp, bottom: 15.sp, left: 15.sp, right: 15.sp),
-                child: Container(
-                  height: 10.h,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: Color(0xFFFEFEFE)),
-                    child: DropdownButton(
-                      value: _selectedValue,
-                      elevation: 10,
-                      style: TextStyle(color: Colors.black),
-                      onChanged: (String? value) {
-                        setState(() {
-                          _selectedValue = value!;
-                        });
-                      },
-                      isExpanded: true,
-                      padding: EdgeInsets.all(20.0.sp),
-                      iconEnabledColor: Color(0xFFFFBC57),
-                      iconSize: 35,
-                      underline: Container(),
-                      // Remove the underline
-                      dropdownColor: Colors.white,
-                      items: _allUnits.map((String value) {
-                        return DropdownMenuItem(
-                          value: value,
-                          child: Center(
-                              child: Text(value,
-                                  style: GoogleFonts.actor(
-                                    textStyle: TextStyle(fontSize: 20.sp),
-                                    fontWeight: FontWeight.bold
-                                  ))),
-                        );
-                      }).toList(),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(top: 20.sp, left: 15.sp, right: 15.sp),
+                    child: Container(
+                      width: screenWidth,
+                      height: 15.h,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(8.0),
+                          color: Color(0xFFFFF1D4)),
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 20.sp,
+                            bottom: 20.sp,
+                            left: 20.sp,
+                            right: 20.sp),
+                        child: Container(
+                          height: 10.h,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(15.0),
+                              color: Color(0xFFFEFEFE)),
+                          child: DropdownButton(
+                            value: _selectedUnitValue,
+                            elevation: 10,
+                            style: TextStyle(color: Colors.black),
+                            onChanged: (String? value) {
+                              setState(() {
+                                _selectedUnitValue = value!;
+                              });
+                            },
+                            isExpanded: true,
+                            padding: EdgeInsets.all(20.0.sp),
+                            iconEnabledColor: Color(0xFFFFBC57),
+                            iconSize: 35,
+                            underline: Container(),
+                            // Remove the underline
+                            dropdownColor: Colors.white,
+                            items: _allUnits.map((String value) {
+                              return DropdownMenuItem(
+                                value: value,
+                                child: Center(
+                                    child: Text(value,
+                                        style: GoogleFonts.actor(
+                                            textStyle:
+                                                TextStyle(fontSize: 20.sp),
+                                            fontWeight: FontWeight.bold))),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-
-          ]),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(top: 20.sp, left: 15.sp, right: 15.sp),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: screenWidth,
+                          height: 18.h,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(8.0),
+                              color: Color(0xFFFFF1D4)),
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                top: 15.sp,
+                                bottom: 15.sp,
+                                left: 15.sp,
+                                right: 15.sp),
+                            child: Container(
+                              height: 18.h,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  color: Color(0xFFFEFEFE)),
+                              child: Column(
+                                children: [
+                                  TextField(
+                                    maxLines: 1,
+                                    textAlign: TextAlign.center,
+                                    keyboardType: TextInputType.number,
+                                    style: TextStyle(
+                                      fontSize: 20.sp,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: DropdownButton(
+                                      value: _selectedLengthValue,
+                                      elevation: 10,
+                                      style: TextStyle(color: Colors.black),
+                                      onChanged: (String? value) {
+                                        setState(() {
+                                          _selectedLengthValue = value!;
+                                        });
+                                      },
+                                      isExpanded: true,
+                                      padding: EdgeInsets.all(20.0.sp),
+                                      iconEnabledColor: Color(0xFFFFBC57),
+                                      iconSize: 35,
+                                      underline: Container(),
+                                      // Remove the underline
+                                      dropdownColor: Colors.white,
+                                      items: _lengthUnits.map((String value) {
+                                        return DropdownMenuItem(
+                                          value: value,
+                                          child: Align(
+                                              alignment: Alignment.centerLeft,
+                                              child: Text(value,
+                                                  style: GoogleFonts.actor(
+                                                      textStyle: TextStyle(
+                                                          fontSize: 18.sp),
+                                                      fontWeight:
+                                                          FontWeight.bold))),
+                                        );
+                                      }).toList(),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.sp),
+                          child: Container(
+                            width: screenWidth,
+                            height: 18.h,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(8.0),
+                                color: Color(0xFFFFF1D4)),
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                  top: 15.sp,
+                                  bottom: 15.sp,
+                                  left: 15.sp,
+                                  right: 15.sp),
+                              child: Container(
+                                height: 18.h,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.rectangle,
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Color(0xFFFEFEFE)),
+                                child: Column(
+                                  children: [
+                                    TextField(
+                                      maxLines: 1,
+                                      textAlign: TextAlign.center,
+                                      keyboardType: TextInputType.number,
+                                      style: TextStyle(
+                                        fontSize: 20.sp,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: DropdownButton(
+                                        value: _selectedLengthValue,
+                                        elevation: 10,
+                                        style: TextStyle(color: Colors.black),
+                                        onChanged: (String? value) {
+                                          setState(() {
+                                            _selectedLengthValue = value!;
+                                          });
+                                        },
+                                        isExpanded: true,
+                                        padding: EdgeInsets.all(20.0.sp),
+                                        iconEnabledColor: Color(0xFFFFBC57),
+                                        iconSize: 35,
+                                        underline: Container(),
+                                        // Remove the underline
+                                        dropdownColor: Colors.white,
+                                        items: _lengthUnits.map((String value) {
+                                          return DropdownMenuItem(
+                                            value: value,
+                                            child: Align(
+                                                alignment: Alignment.centerLeft,
+                                                child: Text(value,
+                                                    style: GoogleFonts.actor(
+                                                        textStyle: TextStyle(
+                                                            fontSize: 18.sp),
+                                                        fontWeight:
+                                                            FontWeight.bold))),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(25.0.sp),
+                          child: ElevatedButton(
+                            child: Text("calculate",
+                                style: GoogleFonts.actor(
+                                    textStyle: TextStyle(
+                                        fontSize: 18.sp),
+                                    fontWeight:
+                                    FontWeight.bold,
+                                  color: Colors.black
+                                ),
+                            ),
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFFFFBC57),
+                              fixedSize: Size(200, 50),
+                              elevation: 12,
+                              shape: BeveledRectangleBorder(),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ]),
+          ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
