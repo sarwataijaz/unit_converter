a21dfb4ec1ef9a1d8d8cf85929117d697e818085
sarwataijaz <sarwataijaaz@gmail.com>
2024-07-16 06:36:18 +0500

    phull ui completed, calculationlogic needed

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _selectedUnitValue = 'Length';
-  String _selectedLengthValue = 'Meter';
+  String _selectedSubUnitValue1 = 'Meter';
+  String _selectedSubUnitValue2 = 'Centimeter';
 
-  List<String> _allUnits = ['Length', 'Area', 'Mass', 'Pressure'];
+  List<String> _allUnits = [
+    'Length',
+    'Time',
+    'Mass',
+    'Pressure',
+    'Temperature'
+  ];
+
   List<String> _lengthUnits = [
     'Meter',
     'Centimeter',
@@ -45,11 +53,82 @@ class _MyHomePageState extends State<MyHomePage> {
     'Kilometer'
   ];
 
+  List<String> _massUnits = [
+    'Kilogram',
+    'Gram',
+    'Milligram',
+    'Pound'
+  ];
+
+  List<String> _pressureUnits = [
+    'Standard Atmosphere',
+    'Pascal',
+    'Torr',
+    'Pound Per Square Inch',
+  ];
+
+  List<String> _timeUnits = [
+    'Hour',
+    'Second',
+    'Millisecond',
+    'Microsecond',
+    'Day',
+    'Week'
+  ];
+
+  List<String> _temperatureUnits = [
+    'Degree Celsius',
+    'Fahrenheit',
+    'Kelvin',
+  ];
+
+  List<String> _currentList = [''];
+
+  @override
+  void initState() {
+    super.initState();
+    _currentList = _lengthUnits;
+    _selectedUnitValue = 'Length';
+    _selectedSubUnitValue1 = _currentList[0];
+    _selectedSubUnitValue2 = _currentList[1]; // Ensure this is valid for initial load
+  }
+
+  void _checkUnits(String value) {
+
+    if (value == 'Mass') {
+      setState(() {
+        _currentList = _massUnits;
+      });
+    } else if(value == 'Time') {
+      setState(() {
+        _currentList = _timeUnits;
+      });
+    } else if(value == 'Pressure') {
+      setState(() {
+        _currentList = _pressureUnits;
+      });
+    } else if(value == 'Temperature') {
+      setState(() {
+        _currentList = _temperatureUnits;
+      });
+    } else {
+      setState(() {
+        _currentList = _lengthUnits;
+      });
+    }
+    print("List: $_currentList");
+    _selectedSubUnitValue1 = _currentList[0];
+    print(_selectedSubUnitValue1);
+    _selectedSubUnitValue2 = _currentList[1];
+    print(_selectedSubUnitValue2);
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
+     print("Start: $_currentList");
     return Scaffold(
       backgroundColor: Color(0xFFFEDD9E),
       body: SingleChildScrollView(
@@ -144,6 +223,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             onChanged: (String? value) {
                               setState(() {
                                 _selectedUnitValue = value!;
+                                _checkUnits(_selectedUnitValue);
                               });
                             },
                             isExpanded: true,
@@ -176,7 +256,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       children: [
                         Container(
                           width: screenWidth,
-                          height: 18.h,
+                          height: 19.h,
                           decoration: BoxDecoration(
                               shape: BoxShape.rectangle,
                               borderRadius: BorderRadius.circular(8.0),
@@ -206,12 +286,12 @@ class _MyHomePageState extends State<MyHomePage> {
                                   ),
                                   Expanded(
                                     child: DropdownButton(
-                                      value: _selectedLengthValue,
+                                      value: _selectedSubUnitValue1,
                                       elevation: 10,
                                       style: TextStyle(color: Colors.black),
                                       onChanged: (String? value) {
                                         setState(() {
-                                          _selectedLengthValue = value!;
+                                          _selectedSubUnitValue1 = value!;
                                         });
                                       },
                                       isExpanded: true,
@@ -221,17 +301,16 @@ class _MyHomePageState extends State<MyHomePage> {
                                       underline: Container(),
                                       // Remove the underline
                                       dropdownColor: Colors.white,
-                                      items: _lengthUnits.map((String value) {
+                                      items: _currentList.map((String value) {
+                                        print(_currentList);
                                         return DropdownMenuItem(
                                           value: value,
-                                          child: Align(
-                                              alignment: Alignment.centerLeft,
+                                          child: Center(
                                               child: Text(value,
                                                   style: GoogleFonts.actor(
-                                                      textStyle: TextStyle(
-                                                          fontSize: 18.sp),
-                                                      fontWeight:
-                                                          FontWeight.bold))),
+                                                      textStyle:
+                                                      TextStyle(fontSize: 20.sp),
+                                                      fontWeight: FontWeight.bold))),
                                         );
                                       }).toList(),
                                     ),
@@ -245,7 +324,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           padding: EdgeInsets.only(top: 20.sp),
                           child: Container(
                             width: screenWidth,
-                            height: 18.h,
+                            height: 19.h,
                             decoration: BoxDecoration(
                                 shape: BoxShape.rectangle,
                                 borderRadius: BorderRadius.circular(8.0),
@@ -275,12 +354,12 @@ class _MyHomePageState extends State<MyHomePage> {
                                     ),
                                     Expanded(
                                       child: DropdownButton(
-                                        value: _selectedLengthValue,
+                                        value: _selectedSubUnitValue2,
                                         elevation: 10,
                                         style: TextStyle(color: Colors.black),
                                         onChanged: (String? value) {
                                           setState(() {
-                                            _selectedLengthValue = value!;
+                                            _selectedSubUnitValue2 = value!;
                                           });
                                         },
                                         isExpanded: true,
@@ -290,17 +369,15 @@ class _MyHomePageState extends State<MyHomePage> {
                                         underline: Container(),
                                         // Remove the underline
                                         dropdownColor: Colors.white,
-                                        items: _lengthUnits.map((String value) {
+                                        items: _currentList.map((String value) {
                                           return DropdownMenuItem(
                                             value: value,
-                                            child: Align(
-                                                alignment: Alignment.centerLeft,
+                                            child: Center(
                                                 child: Text(value,
                                                     style: GoogleFonts.actor(
-                                                        textStyle: TextStyle(
-                                                            fontSize: 18.sp),
-                                                        fontWeight:
-                                                            FontWeight.bold))),
+                                                        textStyle:
+                                                        TextStyle(fontSize: 20.sp),
+                                                        fontWeight: FontWeight.bold))),
                                           );
                                         }).toList(),
                                       ),
@@ -317,7 +394,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             child: Text("calculate",
                                 style: GoogleFonts.actor(
                                     textStyle: TextStyle(
-                                        fontSize: 18.sp),
+                                        fontSize: 20.sp),
                                     fontWeight:
                                     FontWeight.bold,
                                   color: Colors.black
